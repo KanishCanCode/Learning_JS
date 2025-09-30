a588b9fce75e9ff0fd2a1bc57375b8f3a11443f5
Kanish <147413494+KanishCanCode@users.noreply.github.com>
2025-10-01 01:01:24 +0530

    Add files via upload

---
+++ Terninary_op_11/index.js
@@ -0,0 +1,20 @@
+/*let age =11;
+let message=age>18 ? "you are an adult": "you are a minor";
+console.log(message)
+
+
+let time = 17;
+let greeting = time<12 ? "good morning" : "Good afternoon"
+console.log(greeting);
+
+
+
+let isStudent = true;
+let message=isStudent ? "You are a student":"You are not a student";
+console.log(message)
+
+*/
+
+let purchaseAmount = 1550;
+let discount = purchaseAmount>=100 ? 10:0;
+console.log(`Your total is $${purchaseAmount-purchaseAmount*(discount/100)}`);
