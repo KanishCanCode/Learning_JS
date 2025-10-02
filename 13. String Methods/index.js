3906af20dd4fdaed92211cc94e40b0fbbeec9a4a
Kanish <147413494+KanishCanCode@users.noreply.github.com>
2025-10-03 01:54:51 +0530

    Add files via upload

---
+++ 13. String Methods/index.js
@@ -0,0 +1,33 @@
+/*
+
+let usename = " uday ";
+usename.charAt(0);
+console.log(usename.charAt(1));
+console.log(usename.indexOf("u"));
+console.log(usename.lastIndexOf("u"));
+console.log(usename.length);
+console.log(usename.trim());
+console.log(usename.toUpperCase());
+console.log(usename.toLocaleLowerCase());
+console.log(usename.repeat(3));
+console.log(usename.startsWith(" "));
+console.log(usename.endsWith(" "));
+console.log(usename.includes("u"));
+
+
+
+
+let username = "kanish";
+
+let result = username.startsWith("k");
+
+if(result){
+    console.log("Your name starts with K");
+}else{
+    console.log("Your name does not begin with K");
+}
+*/
+
+let PNo = "91-899-8921";
+PNo=PNo.padEnd(15,"0");
+console.log(PNo)
