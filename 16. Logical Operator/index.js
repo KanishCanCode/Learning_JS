cf934a7fcc928f3a364af0c8a4a46e4f4c984009
Kanish <147413494+KanishCanCode@users.noreply.github.com>
2025-10-03 01:56:09 +0530

    Add files via upload

---
+++ 16. Logical Operator/index.js
@@ -0,0 +1,12 @@
+let temp = 200;
+if(temp>0 && temp<=30){
+    console.log("the weather is good");
+}
+else{
+    console.log("the weather is bad")
+}
+
+
+// AND - > &&
+//OR -> ||
+//NOT->!
---
+++ 17. Strict Equality/index.js
@@ -0,0 +1,11 @@
+/*
+= assignment operator
+== comaprison operator
+=== strict equlaity operator(Value and datatype)
+!= inequlity operator
+!== strict inequality operator
+*/
+
+const Pi= 3.14;
+let p = Pi ==="3.14" ? "True":"False"
+console.log(p)
